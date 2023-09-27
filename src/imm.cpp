58da7f3d26d74c0bfff11d19da6700c2be9a7127
chenxlong3 <1226608021@qq.com>
2023-09-27 17:25:25 +0800

    args

--- src/imm.cpp
+++ src/imm.cpp
@@ -1,7 +1,7 @@
 #define HEAD_INFO
 
 #include "sfmt/SFMT.h"
-#include "head.h"
+#include "head.h" 
 
 class Argument{
 public:
@@ -11,6 +11,8 @@ public:
     double epsilon1;
     string model;
     double T;
+    bool truncated=false;
+    int rand_seed=2023;
 };
 
 #include "graph.h"
@@ -19,21 +21,37 @@ public:
 
 
 
-void run_with_parameter(InfGraph &g, const Argument & arg)
+void run_with_parameter(InfGraph &g, Argument & arg, int64 num_RRsets=50000)
 {
         cout << "--------------------------------------------------------------------------------" << endl;
         cout << arg.dataset << " k=" << arg.k << " epsilon=" << arg.epsilon <<   " " << arg.model << " " << arg.epsilon1 << endl;
-
+        double total_eps = arg.epsilon + arg.epsilon1;
+        string folder_name =  arg.dataset.substr(0, arg.dataset.size()-1) + "_k_" + to_string(arg.k) + "_eps_" + to_string(total_eps) + "_rand_" +to_string(arg.rand_seed);
+        string folder_path = "./exp_data/" + folder_name + "/";
+        cout << folder_path << endl;
+        make_dir(folder_path);
+        string log_file_name;
+        if (arg.truncated)
+        {
+            log_file_name = "eps_" + to_string(arg.epsilon) + "_eps1_" + to_string(arg.epsilon1) + ".txt";
+        }
+        else log_file_name = "no_trunc.txt";
+        string log_filepath = folder_path + log_file_name;
+        ofstream logfile(log_filepath);
+        
         Imm::InfluenceMaximize(g, arg);
 
         INFO(g.seedSet);
-       
-    Timer::show();
+        arg.truncated = false;
+        INFO("Influence:");
+        double eval_inf = g.comp_inf_by_cov(num_RRsets, arg);
+        INFO(eval_inf);
+    logfile << "Influence Evaluation: " << eval_inf << endl;
+    Timer::show(logfile);
 }
 void Run(int argn, char **argv)
 {
     Argument arg;
-
 
     for (int i = 0; i < argn; i++)
     {
@@ -52,8 +70,16 @@ void Run(int argn, char **argv)
             arg.T = atof(argv[i + 1]);
         if (argv[i] == string("-k")) 
             arg.k = atoi(argv[i + 1]);
+        if (argv[i] == string("-RAND_SEED")) 
+            arg.rand_seed = atoi(argv[i + 1]);
         if (argv[i] == string("-model"))
             arg.model = argv[i + 1];
+        if (argv[i] == string("-truncated"))
+        {
+            string str_trunc = argv[i + 1];
+            arg.truncated = stob(str_trunc);
+        }
+            
     }
     ASSERT(arg.dataset != "");
     ASSERT(arg.model == "IC" || arg.model == "LT" || arg.model == "TR" || arg.model=="CONT");
